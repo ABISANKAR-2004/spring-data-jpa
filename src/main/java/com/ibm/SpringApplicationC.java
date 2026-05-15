1c7e0a47669a9f728145e59c6486f62d9d0a9599
Abisankar S <abi.sankar@ibm.com>
2026-05-15 15:23:57 +0530

    main class updation

--- src/main/java/com/ibm/SpringApplicationC.java
+++ src/main/java/com/ibm/SpringApplicationC.java
@@ -2,10 +2,18 @@ package com.ibm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringApplicationC {
 
+public class SpringApplicationC extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringApplicationC.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApplicationC.class, args);
 	}

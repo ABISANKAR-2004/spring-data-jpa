011b085dfbf7b2d7acee81466aabb1edb4ce6a6a
Abisankar S <abi.sankar@ibm.com>
2026-05-15 15:12:14 +0530

    test class modified

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,19 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
 class DemoApplicationTests {
-
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 }
